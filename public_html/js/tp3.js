391ff862b3a951e2367adcbacc01bd1c72120d5a
Marine ANGELONI <marine.angeloni@live.com>
2016-10-08 15:49:43 +0200

    Question3: js function

---
+++ public_html/js/tp3.js
@@ -0,0 +1,11 @@
+// Méthode appelée dès que l'utilisateur modifie sa saisie.
+function retrieve() {
+    var saisie = document.getElementById("bsearch").value;
+    retrieve(saisie);
+}
+
+
+function retrieve(saisie) {
+    
+}
+
